c256c0b6637e3e46c66e5c8984c546dc0d7822da
Orly Pantaleon <orlypantaleon25@gmail.com>
2025-03-24 14:34:44 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

--- exercise_01.py
+++ exercise_01.py
@@ -3,12 +3,14 @@ decimal_value = 3.14
 message = "Hello, Python!"
 is_active = True 
 result = None
-count = 10
+Count = 15
 total_count = 20
 
 print("Integer (count):", count)
+print("Integer (Count):", Count)
 print("Integer (total_count):", total_count)
 print("Decimal:", decimal_value)
 print("Text:", message)
 print("Boolean:", is_active)
 print("None Value:", result)
+print("Sum: 8.00")
